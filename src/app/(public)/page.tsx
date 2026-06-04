7e348347e8e488c83983efddb846d23e836e2694
jeremygsz <jeremygrosz@gmail.com>
2026-06-04 16:32:24 +0200

    db connection actu

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
             <EquipesSection />
             <AgendaSection />
             <ClubSection />
-            <PartenairesSection />
+            {/*<PartenairesSection />*/}
         </>
     );
 }
